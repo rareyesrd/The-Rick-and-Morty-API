69257f8b32b01506cc2883453fa16f8c2d4f329a
Rafael Reyes <rafaelalexanderr5@gmail.com>
2020-11-19 23:13:58 -0400

    Finished - Api consumed

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 var app = new Vue({
-  el: "#app",
-  data: {
-      info: null
-  },
-  mounted () {
-    axios
-      .get('https://rickandmortyapi.com/api/character/')
-      .then(response => (this.info = response.data.results))
+    el: "#app",
+    data: {
+      info: null,
+      episodes: null,
+      search: null
+    },
+    methods: {
+      getInfo() {
+        axios
+          .get('https://rickandmortyapi.com/api/character/')
+          .then(response => (this.info = response.data.results))
+          .catch(e => console.log(e))
+      },
+      getEpisode() {
+        axios
+          .get('https://rickandmortyapi.com/api/episode/')
+          .then(response => (this.episodes = response.data.results))
+          .catch(e => console.log(e))
+      }
+    },
+  mounted() {
+    this.getInfo();
+    this.getEpisode();
   }
 });
